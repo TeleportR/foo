229145f1738b7444db5d474b70c3d2d91bd6340c
orangeman <flo@teleportr.org>
2014-09-14 21:23:09 +0200

    gradle directory structure

---
+++ src/main/java/de/fahrgemeinschaft/Secret.java
@@ -0,0 +1,26 @@
+package de.fahrgemeinschaft;
+
+public class Secret {
+    public static final String APIKEY =
+            "4f3e25547734253b4c2b522b2d3568353e3d" +
+            "54714e58672a4f667d714d626338287" +
+            "03e6e645a7c46343d6623516831482d" +
+            "62626e7b27724041425569252f4da9";
+
+    public static final String API_KEY =
+            "AIzaSyBgVk7KLTeWuJgU9LSLc3JhKkphMgh_f1w";
+
+    public static final String LICENSE_KEY =
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKC" +
+            "AQEAk8Mh1NQlFJ7gHHmt/eCKYVi33At/P0mCOA+v" +
+            "qqG1jd2iNRLnn1/b3VayKMLlkgTPR7rqvO53IdqX" +
+            "ZQufLDeJ60ziVYKTndZt/NUJAp5tEDazxPIQxCJ5" +
+            "kcAoJvkfijpy91B20s6rPpZ0Md4fQb6caEu3rC9v" +
+            "Bu8jllUAY6oxTqxGz7UxiZPK8eDmitGoTHuZi/wN" +
+            "QCRagHLTtfDtVsTX8yeTAH4uJjCn/xP3nHQOuYEk" +
+            "nKupw/xnLmHIxNXkaOvtggyFOt3HnaWpTuPXe853" +
+            "5mkJ7ln4CYeoMRHOsu0EpIv/WLgjmORO/GIiphom" +
+            "NFtH41NJ2EZrpLruu4yZXpmpLQIDAQAB";
+
+    public static final String DONATE_KEY = "moo";
+}
